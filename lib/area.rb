34cf8a1edc4e83cd3ef858d5b8ba404446d2aa67
Jonathan Andres <jcabas.ing@gmail.com>
2015-06-15 02:47:20 -0500

    Refactor area class

--- lib/area.rb
+++ lib/area.rb
@@ -5,28 +5,15 @@ class Area
     @x, @y, @color, @matrix = x-1, y-1, color, matrix
   end
 
-  def fill(x = @x,y = @y, oldcolor = nil)
-    if oldcolor == nil
-      oldcolor = matrix[y, x]
-    end
-    return matrix if matrix[y, x] != oldcolor
+  def fill(x = @x,y = @y, default = nil)
+    default = matrix[y, x] if default == nil
+    return matrix if matrix[y, x] != default
 
     matrix[y, x] = color
 
-    if x > 0 # left
-      fill(x - 1, y, oldcolor)
-    end
-
-    if y > 0 # up
-      fill(x, y - 1, oldcolor)
-    end
-
-    if x <= matrix.column_count - 1 # right
-      fill(x + 1, y, oldcolor)
-    end
-
-    if y <= matrix.row_count - 1 # down
-      fill(x, y + 1, oldcolor)
-    end
+    fill(x - 1, y, default) if x > 0
+    fill(x, y - 1, default) if y > 0
+    fill(x + 1, y, default) if x <= matrix.column_count - 1
+    fill(x, y + 1, default) if y <= matrix.row_count - 1
   end
 end
