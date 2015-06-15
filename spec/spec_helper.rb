8cc4aa05d3852f10a69212a649c95c3d67d9de74
Jonathan Andres <jcabas.ing@gmail.com>
2015-06-14 19:57:57 -0500

    Refactor tests
    - Create support file and move fixtures there

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,6 @@ require_relative '../lib/line.rb'
 require_relative '../lib/rectangle.rb'
 require_relative '../lib/area.rb'
 require_relative '../lib/canvas.rb'
+
+require 'support'
+include MatrixFixtureSupport
